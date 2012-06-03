539cbb4c9b86c9993da0092deaf8e8f5719be0ea
Dave Erickson <david.erickson@gmail.com>
2012-06-02 20:26:34 -0400

    committing node porion of project as stub

---
+++ dinotrap-node/app.js
@@ -0,0 +1,45 @@
+/**
+ * DinoTrap
+ * A localized game involving dinosaurs, traps, etc ...
+ */
+
+// ######## Module dependencies.
+
+var express = require('express');
+var app = module.exports = express.createServer();
+var io = require('socket.io').listen(app);
+
+// ######## Configuration
+
+app.configure(function(){
+  //app.set('views', __dirname + '/views');
+  //app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+
+// ######## Socket.IO
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+// ######## Launch
+
+app.listen(9060, function(){
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
